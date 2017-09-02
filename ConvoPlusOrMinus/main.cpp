554607c81910e244a63c72dcb5ba1ae5e0819a8e
PercMain <bl9131@mac.com>
2017-09-01 20:23:32 -0400

    Initial Commit

---
+++ ConvoPlusOrMinus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConvoPlusOrMinus
+//
+//  Created by Ben Lewson on 9/1/17.
+//  Copyright © 2017 Ben Lewson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
